c9df6609fcbfaab52af003b8aafeed0922861d23
EC2 Default User <ec2-user@ip-172-31-3-162.ap-northeast-1.compute.internal>
2023-08-28 12:06:38 +0000

    投稿機能

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   def show
+   @user = User.find(params[:id])
   end
 
   def edit
@@ -10,4 +11,10 @@ class Public::UsersController < ApplicationController
 
   def favorites
   end
+  
+    private
+  def user_params
+    params.require(:user).permit(:name, :gender, :age, :profile_image,)
+  end
+  
 end
