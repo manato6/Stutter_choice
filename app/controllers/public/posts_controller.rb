99ff2040326e743ebd106780a2307141f3ae8feb
EC2 Default User <ec2-user@ip-172-31-3-162.ap-northeast-1.compute.internal>
2023-09-21 11:51:20 +0000

    レイアウト

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -3,13 +3,13 @@ class Public::PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id
-    @post.save
-    redirect_to user_path(current_user)
-  end
-  
+  # def create
+  #   @post = Post.new(post_params)
+  #   @post.user_id = current_user.id
+  #   @post.save
+  #   redirect_to user_path(current_user)
+  # end
+
   def index
     @posts = params[:job_id].present? ? Job.find(params[:job_id]).posts : Post.all
     if params[:keyword]
@@ -26,13 +26,13 @@ class Public::PostsController < ApplicationController
     @comments = @post.comments
     @comment = current_user.comments.new
   end
-  
+
    def create
     @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_back(fallback_location: root_path)  #コメント送信後は、一つ前のページへリダイレクトさせる。
+      redirect_to user_path(current_user) # 投稿に成功した時ユーザーページに遷移する
     else
-      redirect_back(fallback_location: root_path)  #同上
+      render :new # 投稿に失敗した時もう一度フォームに戻る
     end
    end
 
